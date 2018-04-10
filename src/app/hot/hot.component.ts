e2ca69ad2f1ded9bd8e28779292ef2559ce299cf
ruanjianning <ruanjianning@qtone.cn>
2018-04-10 18:01:10 +0800

    ng-trip基础

--- src/app/hot/hot.component.ts
+++ src/app/hot/hot.component.ts
@@ -1,7 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import {Observable} from "rxjs/Observable";
-import {HttpClient} from "@angular/common/http";
-import * as _ from 'lodash'
+import {HttpClient, HttpHandler, HttpParams} from "@angular/common/http";
+
+import tripItem from './../../assets/js/trip-list'
 
 @Component({
   selector: 'app-hot',
@@ -12,14 +13,20 @@ export class HotComponent implements OnInit {
 
   dataSour: Observable<any>;
   product: Array<any> = [];
+  banner: Array<any> = [];
+  tripItem:Array<any>;
 
   constructor(private http: HttpClient) { }
 
   ngOnInit() {
-    this.http.get('http://m.breadtrip.com/api/v2/index/').subscribe((data) => {
+    if (tripItem.status === 0) {
+      this.tripItem = tripItem.data.elements;
+    }
+    console.log(this.tripItem);
+    /*this.http.get('http://m.breadtrip.com/api/v2/index/').subscribe((data) => {
       console.log(data);
     });
-    console.log(this.product);
+    console.log(this.product);*/
   }
 
 }
--- src/app/list/list.component.ts
+++ src/app/list/list.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
 
 @Component({
   selector: 'app-list',
@@ -12,4 +12,5 @@ export class ListComponent implements OnInit {
   ngOnInit() {
   }
 
+  @Input() hotTrip: Array<any>;
 }
